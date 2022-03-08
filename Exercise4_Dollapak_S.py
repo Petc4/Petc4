892b52074935c8ac1af02b0a599d2473e4fe02d2
Petc4 <101171618+Petc4@users.noreply.github.com>
2022-03-08 15:40:59 +0700

    Delete Exercise4_Dollapak_S.py

--- Exercise4_Dollapak_S.py
+++
@@ -1,9 +0,0 @@
-FE  = 80
-GE  = 75.8
-ICS = 67.5
-CP  = 90.3
-print("--- Your Score ---")
-print("Foundation English" , FE)
-print("General Business",GE)
-print("Introduction to Computer Systems", ICS)
-print("Computer Programming" , CP)
